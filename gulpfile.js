f355dcc2929f3725cc2559b80be7d6af14172e26
Maicon Santana <msn-maicon@hotmail.com>
2024-02-28 06:04:59 -0300

    init16

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,25 @@
+const gulp = require('gulp');
+
 function funcaoPadrao(callback){
-    console.log("Executado, via Gulp");
-    callback();
+    setTimeout(function(){
+        console.log("Executado, via Gulp"); //subir linha de comando, Alt + direciona pra cima.
+        callback();
+    },2000);
 }
 
+
 function dizOi(callback){
-    console.log("Olá, Gulp");
-    dizTchau();
-    callback();
+    setTimeout (function(){
+        console.log("Olá, Gulp");
+        dizTchau();
+        callback();
+
+    }, 1000);
 }
 
 function dizTchau(){
     console.log("Tchau, Gulp"); //tarefas privadas não são exportadas mas são executadas atravez de outras.
 }
 
-exports.default = funcaoPadrao;
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
