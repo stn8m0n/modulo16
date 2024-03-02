73bd5eb4c798c32a94d5b78b53ca2c11a1a936a4
Maicon Santana <msn-maicon@hotmail.com>
2024-03-02 04:47:01 -0300

    aula16.2final

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function compilaSass(){
-    return gulp.src('./source/styles/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./build/stylus'));
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
 }
 
 function funcaoPadrao(callback){
@@ -31,3 +36,6 @@ function dizTchau(){
 exports.default = gulp.parallel(funcaoPadrao, dizOi);
 exports.dizOi = dizOi;
 exports.sass = compilaSass;
+exports.watch = function() {
+    gulp.watch('./source/styles/*.scss',{ ignoreInitial:false}, gulp.series(compilaSass));
+}
